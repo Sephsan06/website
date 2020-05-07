bd656648ee99b36e1fbc3c7c2f767783a0e85b82
SephSan06 <sephipr06@gmail.com>
2020-05-07 17:06:05 -0400

    add hello3.sh

---
+++ hello3.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+#attemps to create and argument and reply with "Hello World"
+clear
+
+echo -n "Enter your name : "
+read reply
+
+if [[ $reply ]]; then
+  echo "Hello $name, welcome to programming hell"
+  #statements
+fi
